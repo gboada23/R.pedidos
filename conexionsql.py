db1defa87b029bf7f82b0849f885e96f34119d37
gboada23 <gustavoboadalugo@gmail.com>
2024-10-11 16:48:05 -0400

    App v1.0

---
+++ conexionsql.py
@@ -0,0 +1,74 @@
+import pandas as pd
+import urllib.parse
+from sqlalchemy import create_engine
+from sqlalchemy import text
+from mysql.connector import Error
+from sqlalchemy.exc import SQLAlchemyError
+import streamlit as st
+
+class MYSQL:
+    def __init__(self):
+        self.user = st.secrets["mysql"]["user"]
+        self.password = st.secrets["mysql"]["password"]
+        self.host = st.secrets["mysql"]["host"]
+        self.database = st.secrets["mysql"]["database"]
+        # Inicializa la conexión al crear la instancia
+        self.engine = self.create_connection()
+
+    def create_connection(self):
+        # Método para crear y retornar la conexión usando PyMySQL
+        password_encoded = urllib.parse.quote(self.password)
+        connection_string = f'mysql+pymysql://{self.user}:{password_encoded}@{self.host}/{self.database}'
+        try:
+            engine = create_engine(connection_string)
+            return engine
+        except SQLAlchemyError as e:
+            st.error(f"Error al conectar a la base de datos: {e}")
+            return None
+
+
+    def tabla_inventario(self):
+        query = '''
+            SELECT i1.FECHA, i1.unidad, i1.CODIGO, i1.alterno, i1.PRODUCTO, i1.FISICO
+            FROM inventario_dat i1
+            INNER JOIN (
+                SELECT CODIGO, unidad, MAX(FECHA) AS MaxFecha
+                FROM inventario_dat
+                GROUP BY CODIGO
+            ) i2
+            ON i1.CODIGO = i2.CODIGO AND i1.FECHA = i2.MaxFecha;
+        '''
+        return self.fetch_data(query)
+
+    def vista_pedidos(self):
+        query = '''SELECT 
+                        fechapedido as fecha, 
+                        semana,
+                        comedor,
+                        pedido AS nro_pedido,
+                        familia,
+                        descripcion,
+                        cantidad,
+                        costo_dolar,
+                        precio AS precio_dolar,
+                        cantidad * costo_dolar AS costo_total_dolar,
+                        cantidad * precio AS precio_total_dolar
+                    FROM v_pedidos
+                    WHERE fechapedido >= '2024-04-01'
+                    AND costo_dolar != 0
+                    AND precio !=0 ;'''
+        return self.fetch_data(query)
+
+    def fetch_data(self, query):
+        try:
+            if self.engine:
+                # Ejecuta la consulta y convierte el resultado en un DataFrame
+                df = pd.read_sql(query, self.engine)
+                return df
+            else:
+                st.error("No se pudo establecer la conexión con la base de datos.")
+                return None
+        except SQLAlchemyError as e:
+            st.error(f"Error al ejecutar la consulta: {e}")
+            return None
+
---
+++ main.py
@@ -0,0 +1,117 @@
+import streamlit as st
+from conexionsql import MYSQL
+import pandas as pd
+import io
+from datetime import datetime
+#INSTANCIA SQL 
+Mysql = MYSQL()
+# CREAMOS EL DF CON EL METODO DE LA CLASE MYSQL
+df = Mysql.vista_pedidos()
+# CONFIGURAMOS LA PAGINA
+layout = "centered"
+page_title = "Rentabilidad | Camiones"
+page_icon = "🚛"
+
+st.set_page_config(
+    page_title=page_title,
+    page_icon=page_icon,
+    layout=layout)
+
+# CARGAMOS EL CSS
+# AÑADIMOS EL ESTILO
+css_file = "styles/main.css"
+
+with open(css_file) as f:
+    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+col1,  col2, col3 = st.columns((1,3,1))
+with col2:
+    # Mostramos el logo
+    logo = st.image('assets/logo.png', width=350)
+# modifcamos el tipo de dato fecha to_datetime 
+df['fecha'] = pd.to_datetime(df['fecha'], format='%d-%m-%Y')
+
+# AGREGAMOS UNA COLUMNA CON LA SEMANA DEL AÑO
+df['semana'] = df['semana'].astype(int)
+# CREAMOS LA TABLA CON STREAMLIT
+df.sort_values(by=['fecha'],inplace=True)
+
+st.markdown("<h1 style='text-align: center;'>Tablero de Pedidos</h1>", unsafe_allow_html=True)
+
+st.subheader("¿Cómo te gustaría realizar el filtrado?")
+
+selected_week = st.radio("Elija un filtro:", ['Semana', 'Semana y comedor', 'Mes', 'Mes y Comedor',
+                                              'Familia', 'Familia y Comedor', 'Nro de pedido'])
+
+# Función para convertir el dataframe en un archivo de Excel
+def to_excel(df):
+    output = io.BytesIO()
+    with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
+        df.to_excel(writer, index=False, sheet_name='Sheet1')
+        writer.close()  # Cierra y guarda el archivo correctamente
+    processed_data = output.getvalue()
+    return processed_data
+
+# Lista de semanas para el selectbox (del 1 al 52)
+weeks = list(range(1, 53))
+# Obtener la semana actual
+current_week = datetime.now().isocalendar()[1]
+
+# Variable para almacenar el DataFrame filtrado
+df_filtered = pd.DataFrame()  # Inicializa df_filtered
+
+# Función para aplicar los filtros
+def filter_data():
+    # Declarar df_filtered dentro de la función
+    df_filtered = pd.DataFrame()  # Reiniciar df_filtered
+
+    if selected_week == 'Semana':
+        week_filter = st.selectbox("Semana", weeks, index=weeks.index(current_week))
+        df_filtered = df[df['semana'] == week_filter]
+
+    elif selected_week == 'Semana y comedor':
+        week_filter = st.selectbox("Semana", weeks, index=weeks.index(current_week))
+        comer_filter = st.selectbox("Comedor", df['comedor'].unique().tolist())
+        df_filtered = df[(df['semana'] == week_filter) & (df['comedor'] == comer_filter)]
+
+    elif selected_week == 'Mes':
+        month_filter = st.selectbox("Mes", df['fecha'].dt.month_name().unique().tolist())
+        df_filtered = df[df['fecha'].dt.month_name() == month_filter]
+
+    elif selected_week == 'Mes y Comedor':
+        month_filter = st.selectbox("Mes", df['fecha'].dt.month_name().unique().tolist())
+        comer_filter = st.selectbox("Comedor", df['comedor'].unique().tolist())
+        df_filtered = df[(df['fecha'].dt.month_name() == month_filter) & (df['comedor'] == comer_filter)]
+
+    elif selected_week == 'Familia':
+        family_filter = st.selectbox("Familia", df['familia'].unique().tolist())
+        df_filtered = df[df['familia'] == family_filter]
+
+    elif selected_week == 'Familia y Comedor':
+        family_filter = st.selectbox("Familia", df['familia'].unique().tolist())
+        comer_filter = st.selectbox("Comedor", df['comedor'].unique().tolist())
+        df_filtered = df[(df['familia'] == family_filter) & (df['comedor'] == comer_filter)]
+
+    elif selected_week == 'Nro de pedido':
+        pedido_filter = st.text_input("Introduce el Nro de pedido completo: ")
+        df_filtered = df[df['nro_pedido'] == pedido_filter]
+
+    return df_filtered  # Retornar el DataFrame filtrado
+
+# Ejecutar el filtrado dentro del spinner
+with st.spinner('Filtrando datos...'):
+    df_filtered = filter_data()
+
+# Mostrar el DataFrame filtrado si no está vacío
+if not df_filtered.empty:
+    st.dataframe(df_filtered)
+
+    # Botón para descargar el DataFrame filtrado como archivo Excel
+    excel_data = to_excel(df_filtered)
+    st.download_button(
+        label="Descargar en Excel",
+        data=excel_data,
+        file_name='datos_filtrados.xlsx',
+        mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+    )
+else:
+    st.warning("No se encontraron resultados para los filtros seleccionados.")
